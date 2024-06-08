d01d867c20928d04257498a28f399e79435792f4
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-06-08 17:14:02 +0530

    Add files via upload

---
+++ AdityaHalder/console.py
@@ -0,0 +1,75 @@
+import os
+import time
+import logging
+
+from os import getenv
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+
+
+logging.basicConfig(
+    format="[%(asctime)s]:[%(levelname)s]:[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    handlers=[
+        RotatingFileHandler(
+            "logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10
+        ),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+
+
+if os.path.exists("Internal"):
+   load_dotenv("Internal")
+
+
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+
+
+# OPTIONAL VARIABLES
+SESSION_STRING = getenv("SESSION_STRING", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
+
+
+
+# OTHERS VARIABLES
+
+# PM GUARD VARS
+PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🖤 Hey I'm 𝙃𝙚𝙖𝙧𝙩 𝘽𝙚𝙖𝙩's Assistant \n\n💥 Tag My Boss: \n https://t.me/HeartBeat_Muzic.\n\n😈 Otherwise, wait until my Boss comes, don't spam me.. \n You will be AutoBlock(upto 3 messages)**")
+PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 3))
+
+
+
+# USERBOT DEFAULT IMAGE
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://graph.org/file/9ee37cccd7bf55c3ec953.png")
+
+
+
+# Don't Edit This Codes From This Line
+
+LOGGER = logging.getLogger("Genius")
+runtime = time.time()
+
+FLOODXD = {}
+OLD_MSG = {}
+PM_LIMIT = {}
+PLUGINS = {}
+SUDOERS = []
+
+
+COMMAND_HANDLERS = []
+for x in COMMAND_PREFIXES:
+    COMMAND_HANDLERS.append(x)
+COMMAND_HANDLERS.append('')
+
---
+++ AdityaHalder/modules/helpers/inline.py
@@ -0,0 +1,86 @@
+import asyncio
+
+from ... import *
+from .buttons import *
+from .wrapper import *
+from pyrogram.types import *
+
+
+async def help_menu_logo(answer):
+    image = None
+    if image:
+        thumb_image = image
+    else:
+        thumb_image = "https://graph.org/file/c91b3da8a574984f02edb.jpg"
+    button = paginate_plugins(0, plugs, "help")
+    answer.append(
+        InlineQueryResultPhoto(
+            photo_url=f"{thumb_image}",
+            title="🥀 Help Menu ✨",
+            thumb_url=f"{thumb_image}",
+            description=f"🥀 Open Help Menu Of HeartBeat-Assistant ✨...",
+            caption=f"""
+**🥀 Welcome To Help Menu Of
+Genius Userbot » {__version__} ✨...
+
+Click On Below 🌺 Buttons To
+Get Userbot Commands.
+
+🌷Powered By : [𝅗ـﮩ٨ـ𝅽𝅾𓆩𝞖𝘌𝘈𝘙𝘛𝂬♡𝂬𝞑𝘌𝘈𝘛▹ᴴᴮ⸳⸳ⷮ⸳⸳ⷨ𓆪ﮩ٨ـ𝅽𝅾‐𝅘](https://t.me/HeartBeat_Muzic).**
+            """,
+            reply_markup=InlineKeyboardMarkup(button),
+        )
+    )
+    return answer
+
+
+async def help_menu_text(answer):
+    from ... import __version__
+    button = paginate_plugins(0, plugs, "help")
+    answer.append(
+        InlineQueryResultArticle(
+            title="🥀 Help Menu ✨",
+            input_message_content=InputTextMessageContent(f"""
+**🥀 Welcome To Help Menu Of
+Genius Userbot » {__version__} ✨...
+
+Click On Below 🌺 Buttons To
+Get Userbot Commands.
+
+🌷Powered By : [𝅗ـﮩ٨ـ𝅽𝅾𓆩𝞖𝘌𝘈𝘙𝘛𝂬♡𝂬𝞑𝘌𝘈𝘛▹ᴴᴮ⸳⸳ⷮ⸳⸳ⷨ𓆪ﮩ٨ـ𝅽𝅾‐𝅘](https://t.me/HeartBeat_Muzic).**""",
+            disable_web_page_preview=True
+            ),
+            reply_markup=InlineKeyboardMarkup(button),
+        )
+    )
+    return answer
+
+
+async def run_async_inline():
+    @bot.on_inline_query()
+    @inline_wrapper
+    async def inline_query_handler(bot, query):
+        text = query.query
+        if text.startswith("help_menu_logo"):
+            answer = []
+            answer = await help_menu_logo(answer)
+            try:
+                await bot.answer_inline_query(
+                    query.id, results=answer, cache_time=10
+                )
+            except Exception as e:
+                print(str(e))
+                return
+        elif text.startswith("help_menu_text"):
+            answer = []
+            answer = await help_menu_text(answer)
+            try:
+                await bot.answer_inline_query(
+                    query.id, results=answer, cache_time=10
+                )
+            except Exception as e:
+                print(str(e))
+                return
+        else:
+            return
+

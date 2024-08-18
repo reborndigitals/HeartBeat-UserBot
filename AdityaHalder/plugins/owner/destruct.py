cbd73252bc24893e141be10442e502251f642b54
Aswini <aswini.skysea@gmail.com>
2024-08-18 15:45:26 +0530

    destruct.py

--- AdityaHalder/plugins/owner/destruct.py
+++ AdityaHalder/plugins/owner/destruct.py
@@ -4,7 +4,7 @@ from ... import *
 from pyrogram import filters
 
 
-@app.on_message(cdz(["😋🥰", "op", "wow", "super", "😋😍"])
+@app.on_message(cdz(["paken", "op", "wow", "super", "wait"])
     & filters.private & filters.me)
 async def self_media(client, message):
     try:

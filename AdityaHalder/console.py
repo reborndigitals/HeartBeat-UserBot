0e984e89b5bb964c822509deefb2fdee9039e414
Aswini <aswini.skysea@gmail.com>
2024-08-18 16:22:12 +0530

    console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -47,7 +47,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002063555777"))
 
 # Book_Villain
 #API_ID = int(getenv("API_ID", "29650844"))
-API_HASH = getenv("API_HASH", "6154d581d370cbdadd240292c456d7a2")
+#API_HASH = getenv("API_HASH", "6154d581d370cbdadd240292c456d7a2")
 #BOT_TOKEN = getenv("BOT_TOKEN", "7296460979:AAHzGWipkdUIxU6WUYT4hbGqGcK8S6pSqA8")
 #STRING_SESSION = getenv("STRING_SESSION", "BQHEb5wADIXYxwyyPRWHkmVI-aldtLm44cjCdjqzooPdyoquoRaK6HRx1f03Kas09zqXHHZeFeyFSOsqTZA2CMd_2EwblUuU2cd_EV1DeLZnznILN91Mm7N7BCE7QU5FQKZqKuUbBUtAVU418XstovD7dtjRbq4c6YTRz9v5NftM8RUaPdMiix08SoSDPLTMtoVhwW4mONLSOt4drT2OSh1ZEF6h0eOKTfbb2bfSpk-rtCiJsNq8qL_XomIDJKb9LYRZ-m6L_rpd2HKeOz4IFKmuBVrJzSlimmFHzWGMK3mbObaDewK5CQExmtqiBE5GfPFbFH2C1ukjTEEF04BEdvZZ0UontgAAAABEKZxZAA")
 #MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
@@ -56,7 +56,7 @@ API_HASH = getenv("API_HASH", "6154d581d370cbdadd240292c456d7a2")
 
 # Demon_Queen
 #API_ID = int(getenv("API_ID", "20883765"))
-API_HASH = getenv("API_HASH", "8fe7c465d01b1bee8c2156143654ecd1")
+#API_HASH = getenv("API_HASH", "8fe7c465d01b1bee8c2156143654ecd1")
 #BOT_TOKEN = getenv("BOT_TOKEN", "7003201598:AAHAkwHYDgHTZgftpPej_ep2CgLXSTIMZaw")
 #STRING_SESSION = getenv("STRING_SESSION", "BQE-qTUAqDzOvMtdXsq3Fjzjx9mpVtAVxhD5Qj5G7YvQhZATknZBp2NceRdl_7NkYyLw-B2spiTn47O46vINlYBia7KeY9mwUn8qSbtVXE3kFA4yRQ70WaQPcEiaqzla3KHwVwPbFOfR10xwiRsUl9TDd4zXaj1ld4BYVkyWOSmo_vidjPCo1icviFVAzE1_Y8Erm5hImUokK9QYttckGbmuMwJgp7huJiauowPJot06gMfHg5Vm9QTcWFLvLdoZhMAHzqGEmIODGwWwbq0TFILife610fX5q1ee4dzuU3B-dvnzRLoG9TWedSZsA8DI-2ATleEOEqZrr8xKKL0eq0itv7IXcAAAAAGgWO11AA")
 #MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://admin:pass@cluster0.bqss1.mongodb.net/todo?retryWrites=true&w=majority")
@@ -64,7 +64,7 @@ API_HASH = getenv("API_HASH", "8fe7c465d01b1bee8c2156143654ecd1")
 
 # Lisa_Queen
 #API_ID = int(getenv("API_ID", "22431483"))
-API_HASH = getenv("API_HASH", "081facce8bed58c074ede1ab5b0c523c")
+#API_HASH = getenv("API_HASH", "081facce8bed58c074ede1ab5b0c523c")
 #BOT_TOKEN = getenv("BOT_TOKEN", "7411947176:AAGVNuQsKSBQ0ZECQ73nPg0F2ltpk3szzdI")
 #STRING_SESSION = getenv("STRING_SESSION", "BQFWRvsAlV6jO9pEWKi-Apz0eb7r-ZyuJHaQ-m8JiFVx7aZF7zauN_05-yHIvEJovd7o7Ph3Qnja8dhq4IQqEJLn08XSlfj0Gd_G_L5m4DarecsN2_bSAcu3cjmLbTDtFouROX2uyMlpe0f9bNW0UFxi7MYWEeGngBK6IYXqWtwFGQEYTMiNj1o_0HMulaQxXwpB83SIfXR4yJhKxfe9j7eAjmO7HvqNdlZhappf-nqdlVwf0RiuY3v53OBYLJvNaez3-dt00XyZkXhSxbgfJblR-Wr98qmt8VzFGPVSMm_n2ATof5s5kiryMY5-4CCgR75H52NoGastphCiIoXpWgV70RBQAAAAE9MQ-rAA")
 #MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://admin:pass@cluster0.bqss1.mongodb.net/todo?retryWrites=true&w=majority")

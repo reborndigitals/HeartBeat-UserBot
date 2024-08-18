0cb53c104fe776c4f041fce5a1dfae450c2ae432
Aswini <aswini.skysea@gmail.com>
2024-08-18 13:37:21 +0530

    console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -39,7 +39,7 @@ if os.path.exists("Internal"):
 API_ID = int(getenv("API_ID", "13371001"))
 API_HASH = getenv("API_HASH", "f9e4a79850af44c26d736453a95308fc")
 BOT_TOKEN = getenv("BOT_TOKEN", "6773882212:AAGxTyLGiKX-8syKmtgRbHD4Jn5NTiem0DA")
-STRING_SESSION = getenv("STRING_SESSION", "BQB6w5MAddViqx9rSNWBXf1cB8ZgULzrG5yV4Z_Sde9zCtjuCbJVgucg9F3RK6Uq3x4gx22eGYfxOpE_HLivLvPjB6YVg7hGE2pTayMQKjH9KgausKH0cgiTFVrHJ_FHMZb9PFjFvZayLO6VJSihWzULveYiPqisp2SH8rxxv5eTKRgA5WOYc5whgZgQPU2BDI8aaq4axZT1Q5KOgeR7dRpj0oHNzL4eEECDGzdojhr3In0DK5KYjFd1waxL6HxYobLQL7iX4iSGBrStt7TtJnQYavXnLN6Z1kXE7rpNaBe-TZnTl9XDtc2Cb1wt47mg2NernwQ_H_2mvrWk-_dDxXswgi0AIAAAAABMXtJJAA")
+STRING_SESSION = getenv("STRING_SESSION", "BQB6w5MAqoqMLOuUiivDmxzkNQA01seula8MBuWfkeby9xM5h7MrEA6-nc3f2TzNIepXE1vK_nMST04Sv_oQyzlYNobJ9rLJ65Ug_RoliF7msVzU8_6fweL-P10Dh05Tjs9gk_m-uqvW_xu_LJGAi4DwIAe0d0j0ZV9cNs57yS136aUuu2RlTdoQLR0KNTpi2hSvWJccQylC3yci_nMUqQ4eIpohiunKi8optfAw1eA96FyS-Oyejf3VW7n3YF1mj7v2zAcU7tN4yeVj7zphGn8T7wNc6Fihe8t6KY-JCFyhG_EX7Nbt1sBDY74EWlYsn1Ux86xdwTfcFQ3Dvuavp1TdIPQf1QAAAABMXtJJAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002063555777"))
 

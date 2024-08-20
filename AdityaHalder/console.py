4d1c9f1331548b4763f1b79f55b831673e1b53f4
Aswini <aswini.skysea@gmail.com>
2024-08-20 14:36:00 +0530

    console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -31,7 +31,7 @@ if os.path.exists("Internal"):
 # Aishu
 #API_ID = int(getenv("API_ID", "8045459"))
 #API_HASH = getenv("API_HASH", "e6d1f09120e17a4372fe022dde88511b")
-#BOT_TOKEN = getenv("BOT_TOKEN", "7068876137:AAEO6UXBfVcj9BMPXsbdAgApCntlXmYjNTw")
+#BOT_TOKEN = getenv("BOT_TOKEN", "6773882212:AAGxTyLGiKX-8syKmtgRbHD4Jn5NTiem0DA")
 #STRING_SESSION = getenv("STRING_SESSION", "BQDMBnkAYiOVd1XgRlwY7fGlRrmYnn79K2KzLfsXiVAFVRYYfmTzSslg4VMO9zi3V-8UvChSAZJiu4OFnNCHnFYKstELWLGKF4KW70ml64zlF8mgCnAtUPDvJXpJtgrIcjZr-ieZAgda99Giv_ivVbh2V5wLqgSYUbSZ_GKGM4usnJvQFZg6YGMoTMWKQPuZhxphI74-_PyXED5xHpoYpBjbaw00wy8RKJzw3e_NMpXwID5Io1P2uBlx9vbmLcJMHO7DK4YvwsCXQUBNhskfzSiWQqpcVH_eZEW2NN-1uWKDsWbVZPY4PDjraaBVhF5rOi_MVx4oBTAzCV1hT2BGsljeQb7AAAAAEyRzmXAA")
 #MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://zewdatabase:ijoXgdmQ0NCyg9DO@zewgame.urb3i.mongodb.net/ontap?retryWrites=true&w=majority")
 #LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002063555777"))
@@ -39,7 +39,7 @@ if os.path.exists("Internal"):
 # Rajesh
 API_ID = int(getenv("API_ID", "13371001"))
 API_HASH = getenv("API_HASH", "f9e4a79850af44c26d736453a95308fc")
-BOT_TOKEN = getenv("BOT_TOKEN", "6773882212:AAGxTyLGiKX-8syKmtgRbHD4Jn5NTiem0DA")
+BOT_TOKEN = getenv("BOT_TOKEN", "7068876137:AAEO6UXBfVcj9BMPXsbdAgApCntlXmYjNTw")
 STRING_SESSION = getenv("STRING_SESSION", "BQB6w5MAqoqMLOuUiivDmxzkNQA01seula8MBuWfkeby9xM5h7MrEA6-nc3f2TzNIepXE1vK_nMST04Sv_oQyzlYNobJ9rLJ65Ug_RoliF7msVzU8_6fweL-P10Dh05Tjs9gk_m-uqvW_xu_LJGAi4DwIAe0d0j0ZV9cNs57yS136aUuu2RlTdoQLR0KNTpi2hSvWJccQylC3yci_nMUqQ4eIpohiunKi8optfAw1eA96FyS-Oyejf3VW7n3YF1mj7v2zAcU7tN4yeVj7zphGn8T7wNc6Fihe8t6KY-JCFyhG_EX7Nbt1sBDY74EWlYsn1Ux86xdwTfcFQ3Dvuavp1TdIPQf1QAAAABMXtJJAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002063555777"))
